4dcca8edae98379b55266554d62a9436c08364f1
gek007 <kostya.shilkrot@gmail.com>
2025-09-15 21:45:56 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,68 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+from IPython.display import Markdown, display
+from src.website import Website
+
+
+load_dotenv(override=True)
+api_key = os.getenv('OPENAI_API_KEY')
+
+# Check the key
+
+if not api_key:
+    print("No API key was found - please head over to the troubleshooting notebook in this folder to identify & fix!")
+elif not api_key.startswith("sk-proj-"):
+    print("An API key was found, but it doesn't start sk-proj-; please check you're using the right key - see troubleshooting notebook")
+elif api_key.strip() != api_key:
+    print("An API key was found, but it looks like it might have space or tab characters at the start or end - please remove them - see troubleshooting notebook")
+else:
+    print("API key found and looks good so far!")
+
+
+openai = OpenAI()
+
+# message = "Hello, GPT! This is my first ever message to you! Hi!"
+# response = openai.chat.completions.create(model="gpt-4o-mini", messages=[{"role":"user", "content":message}])
+# print(response.choices[0].message.content)
+
+
+# ed = Website("https://edwarddonner.com")
+# print(ed.title)
+# print(ed.text)
+
+system_prompt = "You are an assistant that analyzes the contents of a website \
+and provides a short summary, ignoring text that might be navigation related. \
+Respond in markdown."
+
+def user_prompt_for(website):
+    user_prompt = f"You are looking at a website titled {website.title}"
+    user_prompt += "\nThe contents of this website is as follows; \
+please provide a short summary of this website in markdown. \
+If it includes news or announcements, then summarize these too.\n\n"
+    user_prompt += website.text
+    return user_prompt
+
+def messages_for(website):
+    return [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": user_prompt_for(website)}
+    ]
+
+
+def summarize(url):
+    website = Website(url)
+    response = openai.chat.completions.create(
+        model = "gpt-4o-mini",
+        messages = messages_for(website)
+    )
+    return response.choices[0].message.content
+
+
+def display_summary(url):
+    summary = summarize(url)
+    print(summary)
+    #display(Markdown(summary))
+
+
+display_summary("https://edwarddonner.com")
